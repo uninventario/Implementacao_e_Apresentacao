dc3b1278a660e4c5b20a0e914ade069639efd150
Luisa Marzinette <luisamarzinette@outlook.com>
2023-12-04 09:39:40 -0300

    Maquina e Usuario

--- uninventario/src/main/java/com/uninventario/uninventario/domain/database/Database.java
+++ uninventario/src/main/java/com/uninventario/uninventario/domain/database/Database.java
@@ -10,6 +10,8 @@ public class Database {
     static Maquina[] maquinas = new Maquina[0];
     static Laboratorio[] laboratorios = new Laboratorio[0];
 
+
+    // Laboratorio
     public static void adicionarLaboratorio(Laboratorio laboratorio) {
         Laboratorio[] newLaboratories = new Laboratorio[Database.laboratorios.length + 1];
 
@@ -66,4 +68,120 @@ public class Database {
       public static void resetarLaboratorio() {
         Database.laboratorios = new Laboratorio[0];
       }
+
+
+    // Maquina
+
+      public static void adicionarMaquina(Maquina maquina) {
+        Maquina[] newMaquinas = new Maquina[Database.maquinas.length + 1];
+        // Loop de percusão básico
+        for (int i = 0; i < Database.maquinas.length; i++) {
+            newMaquinas[i] = Database.maquinas[i];
+        }
+        newMaquinas[newMaquinas.length - 1] = maquina;
+        Database.maquinas = newMaquinas;
+    }
+
+    public static Maquina[] buscarTodasMaquinas() {
+        return Database.maquinas;
+    }
+
+    public static Maquina buscarUmaMaquina(Number id) {
+        for (int i = 0; i < Database.maquinas.length; i++) {
+            if (Database.maquinas[i].getId() == id) {
+                return Database.maquinas[i];
+            }
+        }
+        return null;
+    }
+
+    public static void editarMaquina(Maquina MaquinaAtualizada) {
+        for (int i = 0; i < Database.maquinas.length; i++) {
+            if (Database.maquinas[i].getId() == MaquinaAtualizada.getId()) {
+                Database.maquinas[i].setNome(MaquinaAtualizada.getNome());
+                Database.maquinas[i].setModelo(MaquinaAtualizada.getModelo());
+                Database.maquinas[i].setProcessador(MaquinaAtualizada.getProcessador());
+                Database.maquinas[i].setMemoriaRam(MaquinaAtualizada.getMemoriaRam());
+                Database.maquinas[i].setArmazenamento(MaquinaAtualizada.getArmazenamento());
+                Database.maquinas[i].setLaboratorio(MaquinaAtualizada.getLaboratorio());
+                return;
+            }
+        }
+    }
+
+    public static void deletarMaquina(Number id) {
+        Maquina[] newMaquinas = new Maquina[Database.maquinas.length - 1];
+      
+        int novoIndice = 0;
+
+        // Loop de deletar básico
+        for (int i = 0; i < Database.maquinas.length; i++) {
+          if (Database.maquinas[i].getId() != id) {
+            newMaquinas[novoIndice] = Database.maquinas[i];
+            novoIndice++;
+          }
+        }
+
+        Database.maquinas = newMaquinas;
+      }
+
+      public static void resetarMaquina() {
+        Database.maquinas = new Maquina[0];
+      }
+
+      // Usuario 
+
+          public static void adicionarUsuario(Usuario usuario) {
+            Usuario[] newUsuarios = new Usuario[Database.usuarios.length + 1];
+            // Loop de percusão básico
+            for (int i = 0; i < Database.usuarios.length; i++) {
+                newUsuarios[i] = Database.usuarios[i];
+            }
+            newUsuarios[newUsuarios.length - 1] = usuario;
+            Database.usuarios = newUsuarios;
+        }
+    
+        public static Usuario[] buscarTodosUsuarios() {
+            return Database.usuarios;
+        }
+    
+        public static Usuario buscarUmUsuario(Number id) {
+            for (int i = 0; i < Database.usuarios.length; i++) {
+                if (Database.usuarios[i].getId() == id) {
+                    return Database.usuarios[i];
+                }
+            }
+            return null;
+        }
+    
+        public static void editarUsuario(Usuario UsuarioAtualizada) {
+            for (int i = 0; i < Database.usuarios.length; i++) {
+                if (Database.usuarios[i].getId() == UsuarioAtualizada.getId()) {
+                    Database.usuarios[i].setNome(UsuarioAtualizada.getNome());
+                    Database.usuarios[i].setCargo(UsuarioAtualizada.getCargo());
+                    Database.usuarios[i].setMatricula(UsuarioAtualizada.getMatricula());
+                    return;
+                }
+            }
+        }
+    
+        public static void deletarUsuario(Number id) {
+            Usuario[] newUsuarios = new Usuario[Database.usuarios.length - 1];
+          
+            int novoIndice = 0;
+    
+            // Loop de deletar básico
+            for (int i = 0; i < Database.usuarios.length; i++) {
+              if (Database.usuarios[i].getId() != id) {
+                newUsuarios[novoIndice] = Database.usuarios[i];
+                novoIndice++;
+              }
+            }
+    
+            Database.usuarios = newUsuarios;
+          }
+    
+          public static void resetarUsuario() {
+            Database.usuarios = new Usuario[0];
+          }
 }
--- uninventario/src/test/java/com/uninventario/uninventario/UninventarioApplicationTests.java
+++ uninventario/src/test/java/com/uninventario/uninventario/UninventarioApplicationTests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.uninventario.uninventario.domain.database.Database;
 import com.uninventario.uninventario.domain.laboratorio.Laboratorio;
+import com.uninventario.uninventario.domain.maquina.Maquina;
+import com.uninventario.uninventario.domain.usuario.Usuario;
 
 @SpringBootTest
 class UninventarioApplicationTests {
@@ -16,6 +18,21 @@ class UninventarioApplicationTests {
 		Laboratorio laboratorio = new Laboratorio(1, "Laboratório de informática 1", "Predio Central", 2);
 		Database.adicionarLaboratorio(laboratorio);
 		Laboratorio[] resultado = Database.buscarTodosLaboratorios();
+		assertEquals(1, resultado.length);
+	}
+		@Test
+	 	void adicionarMaquinaTest() {
+		Maquina maquina = new Maquina(1, "Máquina 1", "Dell Latitude E6540", "Intel Core i5-5200U", "8 GB", "500 GB", "Laboratório 20");
+		Database.adicionarMaquina(maquina);
+		Maquina[] resultado = Database.buscarTodasMaquinas();
+		assertEquals(1, resultado.length);
+	}
+
+		@Test
+	 	void adicionarUsuarioTest() {
+		Usuario usuario = new Usuario(1, "Daniel Silva Pereira", "Tecnico de informática", 12567);
+		Database.adicionarUsuario(usuario);
+		Usuario[] resultado = Database.buscarTodosUsuarios();
 		assertEquals(1, resultado.length);
 	}
 
